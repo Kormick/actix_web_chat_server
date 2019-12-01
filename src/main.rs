554642c0ea362d73d55bdaca9aeaeb40f415d018
Renat Mukhametzanov <mrkormick@gmail.com>
2019-12-01 22:13:57 +0300

    Adapted data to multi threaded env

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ extern crate actix_web;
 use actix_rt::System;
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use futures::{future::ok, Future};
-use std::cell::{Cell, RefCell};
 use std::collections::HashMap;
 use std::net::SocketAddr;
+use std::sync::{Arc, RwLock};
 
 struct User {
     name: String,
@@ -18,32 +18,29 @@ struct ChatMessage {
 }
 
 struct ChatData {
-    connected_users: RefCell<HashMap<u32, User>>,
-    id_cnt: Cell<u32>,
-    messages: RefCell<Vec<ChatMessage>>,
-    info_cnt: Cell<u32>,
+    connected_users: HashMap<u32, User>,
+    messages: Vec<ChatMessage>,
+    id_cnt: u32,
+    info_cnt: u32,
 }
 
 impl ChatData {
     fn create() -> ChatData {
-        println!("Creating data");
-
         ChatData {
-            connected_users: RefCell::new(HashMap::new()),
-            id_cnt: Cell::new(0),
-            messages: RefCell::new(Vec::new()),
-            info_cnt: Cell::new(0),
+            connected_users: HashMap::new(),
+            messages: Vec::new(),
+            id_cnt: 0,
+            info_cnt: 0,
         }
     }
 
-    fn gen_user_id(&self) -> u32 {
-        self.id_cnt.set(self.id_cnt.get() + 1);
-        self.id_cnt.get()
+    fn gen_user_id(&mut self) -> u32 {
+        self.id_cnt = self.id_cnt + 1;
+        self.id_cnt
     }
 
     fn get_user_id(&self, user: &String) -> Option<u32> {
-        let users = self.connected_users.borrow();
-        for (id, u) in users.iter() {
+        for (id, u) in self.connected_users.iter() {
             if &u.name == user {
                 return Some(*id);
             }
@@ -53,27 +50,22 @@ impl ChatData {
     }
 
     fn is_user_connected(&self, name: &String) -> bool {
-        let users = self.connected_users.borrow();
-        for (_, u) in users.iter() {
-            if &u.name == name {
+        for (_, user) in self.connected_users.iter() {
+            if &user.name == name {
                 return true;
             }
         }
 
-        false
+        return false;
     }
 
-    fn connect_user(&self, id: u32, user: User) {
-        let mut users = self.connected_users.borrow_mut();
-        users.insert(id, user);
+    fn connect_user(&mut self, id: u32, user: User) {
+        self.connected_users.insert(id, user);
     }
 
-    fn add_message(&self, id: u32, mes: String) {
-        let users = self.connected_users.borrow();
-        let name = &users.get(&id).unwrap().name;
-
-        let mut messages = self.messages.borrow_mut();
-        messages.push(ChatMessage {
+    fn add_message(&mut self, id: u32, mes: String) {
+        let name = &self.connected_users.get(&id).unwrap().name;
+        self.messages.push(ChatMessage {
             name: name.to_string(),
             mes,
         });
@@ -82,7 +74,7 @@ impl ChatData {
     fn chat_html(&self) -> String {
         let mut html = String::new();
 
-        for mes in self.messages.borrow().iter() {
+        for mes in self.messages.iter() {
             let text = format!("{}: {}<br/>", mes.name, mes.mes);
             html.push_str(&text);
         }
@@ -93,41 +85,46 @@ impl ChatData {
 
 fn chat(
     _req: HttpRequest,
-    data: web::Data<ChatData>,
+    data: web::Data<Arc<RwLock<ChatData>>>,
 ) -> impl Future<Item = HttpResponse, Error = Error> {
     println!("Chat requested");
 
+    let data = (*data).read().unwrap();
     let chat_html = data.chat_html();
     ok(HttpResponse::Ok().content_type("text/html").body(chat_html))
 }
 
 fn info(
     _req: HttpRequest,
-    data: web::Data<ChatData>,
+    data: web::Data<Arc<RwLock<ChatData>>>,
 ) -> impl Future<Item = HttpResponse, Error = Error> {
+    println!("Info requested");
     let mut info = String::new();
 
+    let mut data = (*data).write().unwrap();
+
     info.push_str("Connected users: <br/>");
-    for (id, user) in data.connected_users.borrow().iter() {
+    for (id, user) in data.connected_users.iter() {
         let text = format!("{} {} {}<br/>", id, user.name, user.ip);
         info.push_str(&text);
     }
 
-    info.push_str(&format!("Id counter: {} <br/>", data.id_cnt.get()));
-    info.push_str(&format!("Info counter: {} <br/>", data.info_cnt.get()));
+    info.push_str(&format!("Id counter: {}<br/>", data.id_cnt));
+    info.push_str(&format!("Info counter: {}<br/>", data.info_cnt));
 
-    data.info_cnt.set(data.info_cnt.get() + 1);
+    data.info_cnt = data.info_cnt + 1;
 
     ok(HttpResponse::Ok().content_type("text/html").body(info))
 }
 
 fn receive_message(
     req: HttpRequest,
-    data: web::Data<ChatData>,
+    data: web::Data<Arc<RwLock<ChatData>>>,
 ) -> impl Future<Item = HttpResponse, Error = Error> {
     let user = req.match_info().get("user").unwrap().to_string();
     let message = req.match_info().get("message").unwrap();
 
+    let mut data = (*data).write().unwrap();
     if !data.is_user_connected(&user) {
         return ok(HttpResponse::BadRequest().body("User not connected"));
     }
@@ -136,15 +133,17 @@ fn receive_message(
         data.add_message(id, message.to_string());
     }
 
-    ok(HttpResponse::Ok().body("OK"))
+    ok(HttpResponse::Ok().body("Message received"))
 }
 
 fn connect_user(
     req: HttpRequest,
-    data: web::Data<ChatData>,
+    data: web::Data<Arc<RwLock<ChatData>>>,
 ) -> impl Future<Item = HttpResponse, Error = Error> {
     let user = String::from(req.match_info().get("user").unwrap());
     let ip = req.head().peer_addr.unwrap();
+
+    let mut data = (*data).write().unwrap();
 
     if data.is_user_connected(&user) {
         return ok(HttpResponse::BadRequest().body("User already connected"));
@@ -155,15 +154,17 @@ fn connect_user(
     let new_user = User { name: user, ip: ip };
     data.connect_user(id, new_user);
 
-    ok(HttpResponse::Ok().body("ok"))
+    ok(HttpResponse::Ok().body("User connected"))
 }
 
 fn main() -> std::io::Result<()> {
     let sys = System::new("chat-server");
 
-    HttpServer::new(|| {
+    let data = Arc::new(RwLock::new(ChatData::create()));
+
+    HttpServer::new(move || {
         App::new()
-            .data(ChatData::create())
+            .data(data.clone())
             .service(web::resource("/chat.html").to_async(chat))
             .service(web::resource("/connect/{user}").to_async(connect_user))
             .service(web::resource("/chat/send/{user}/{message}").to_async(receive_message))
